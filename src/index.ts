e6298de34b76978b7f5bb5f5390fea5dfeb84484
CHON22 <cel.haupenthal.22@lehre.mosbach.dhbw.de>
2023-05-07 21:10:00 +0200

    1rst changes: add constants

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+const characterAmountRangeElement = document.getElementById('characterAmountRange')
+const characterAmountNumberElement = document.getElementById('characterAmountNumber')
+const includeLowercaseElement = document.getElementById('includeLowercase')
+
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const formElement = document.getElementById('passwordGeneratorForm')
+const passwordDisplayElement = document.getElementById('passwordDisplay')
+const copyButtonElement = document.querySelector('#copyButton')
+
+
+
+
