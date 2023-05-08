42eac0a06aa06abcf69d8b1666eeb7f1a2ffe283
CHON22 <cel.haupenthal.22@lehre.mosbach.dhbw.de>
2023-05-08 11:19:25 +0200

    add function isInputElement

---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function isInputElement(e: HTMLElement | EventTarget | null): e is HTMLInputElement {
+    return (e as HTMLInputElement | null)?.value != null || (e as HTMLInputElement | null)?.checked != null
+  }
