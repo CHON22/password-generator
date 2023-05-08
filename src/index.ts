ba6713fb9cb90a8056e2621bb25225ee8b72f997
CHON22 <cel.haupenthal.22@lehre.mosbach.dhbw.de>
2023-05-08 10:30:07 +0200

    add main logic

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,35 @@ const formElement = document.getElementById('passwordGeneratorForm')
 const passwordDisplayElement = document.getElementById('passwordDisplay')
 const copyButtonElement = document.querySelector('#copyButton')
 
+characterAmountNumberElement?.addEventListener('input', syncCharacterAmount)
+characterAmountRangeElement?.addEventListener('input', syncCharacterAmount)
+
+formElement?.addEventListener('submit', e => {
+  e.preventDefault()
+  handleGeneratePassword()
+})
+
+function handleGeneratePassword() {
+  const characterAmountString = isInputElement(characterAmountNumberElement) ? characterAmountNumberElement.value : "10"
+  const characterAmount = parseInt(characterAmountString)
+  const includeUppercase = isInputElement(includeUppercaseElement) ? includeUppercaseElement.checked : false
+  const includeLowercase = isInputElement(includeLowercaseElement) ? includeLowercaseElement.checked : false
+
+  const includeNumbers = isInputElement(includeNumbersElement) ? includeNumbersElement.checked : false
+  const includeSymbols = isInputElement(includeSymbolsElement) ? includeSymbolsElement.checked : false
+  const password = generatePassword({ characterAmount, includeLowercase, includeUppercase, includeNumbers, includeSymbols })
+  if (passwordDisplayElement) passwordDisplayElement.innerText = password
+}
+
+function syncCharacterAmount(e: HTMLElementEventMap["input"]) {
+    if (!isInputElement(e.target)) {
+      return 
+    }
+    const value = e.target.value
+    if (isInputElement(characterAmountNumberElement)) characterAmountNumberElement.value = value
+    if (isInputElement(characterAmountRangeElement)) characterAmountRangeElement.value = value
+  }
+
 
   
   
