d2c42530edff7708e396d8eb3f2737cf7314ad5a
CHON22 <cel.haupenthal.22@lehre.mosbach.dhbw.de>
2023-05-09 17:44:14 +0200

    comment utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+//Überprüfung, ob Element ein HTML-Eingabeelement ist
 export function isInputElement(e: HTMLElement | EventTarget | null): e is HTMLInputElement {
     return (e as HTMLInputElement | null)?.value != null || (e as HTMLInputElement | null)?.checked != null
   }
